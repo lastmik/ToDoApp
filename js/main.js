9ccf586a8cdd32872de9d122930fe65c74b2bc4f
dmitry <dmitry.kravets@itechcraft.com>
2022-02-18 13:10:54 +0200

    Formatting

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
-import{filterAll, 
-  filterActive, 
-  filterCompleted, 
-  addNewToDo, 
-  clearCompleted, 
-  toggleAll, 
-  sortASC, 
-  sortDESC} from './View/View.js';
-  
+import {
+  filterAll,
+  filterActive,
+  filterCompleted,
+  addNewToDo,
+  clearCompleted,
+  toggleAll,
+  sortASC,
+  sortDESC,
+} from "./View/View.js";
+
 export let mainElements = {
-  input : document.querySelector("#input"),
-  todoList : document.querySelector("#todo_list"),
-  clearCompletedElement : document.querySelector("#clear"),
-  toggleAllElement : document.querySelector("#toggle_all"),
-  filters : document.querySelector(".footer"),
-  asc : document.querySelector("#asc"),
-  desc : document.querySelector("#desc"),
-  counter : document.querySelector("#count"),
-  value : ""
-}
-function init(){
+  input: document.querySelector("#input"),
+  todoList: document.querySelector("#todo_list"),
+  clearCompletedElement: document.querySelector("#clear"),
+  toggleAllElement: document.querySelector("#toggle_all"),
+  filters: document.querySelector(".footer"),
+  asc: document.querySelector("#asc"),
+  desc: document.querySelector("#desc"),
+  counter: document.querySelector("#count"),
+  value: "",
+};
+function init() {
   location.hash = "#/";
   document.querySelector("#all").addEventListener("click", filterAll);
   document.querySelector("#active").addEventListener("click", filterActive);
-  document.querySelector("#completed").addEventListener("click", filterCompleted);
+  document
+    .querySelector("#completed")
+    .addEventListener("click", filterCompleted);
 
   mainElements.input.addEventListener("keydown", addNewToDo);
   mainElements.clearCompletedElement.addEventListener("click", clearCompleted);
@@ -31,10 +35,3 @@ function init(){
   mainElements.desc.addEventListener("click", sortDESC);
 }
 window.onload = init;
-
-
-
-
-
-
-
